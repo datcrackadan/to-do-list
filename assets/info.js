4f70538fc7eac44992a352bd56d3886d9e1ad602
Daniel <danielwenger82@gmail.com>
2017-11-06 14:41:36 +0100

    html and css done, js file with json, but don't get it

---
+++ assets/info.js
@@ -0,0 +1,11 @@
+var info = {
+  "user" : "john",
+  "date" : "28/10/2017-12:00",
+  "task" : [
+    {
+      "title" : "my first task",
+      "done" : "false",
+      "done_on" : "null"
+    }
+           ]
+}
