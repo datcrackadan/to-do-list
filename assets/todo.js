cadb016e36e3f970899f8c3ddee121fe5f8c8ed9
Daniel <danielwenger82@gmail.com>
2017-11-08 10:41:54 +0100

    added proper todo.js file

---
+++ assets/todo.js
@@ -0,0 +1,49 @@
+"use strict";
+
+let data = {
+  "user" : [
+    {
+      "name"     : "john",
+      "email"    : "johnsmith@matrix.com",
+      "password" : "sjdnc54ffdf22",
+      "team"     : [],
+      "task"     : []
+    },
+    {
+      "name"      : "carlos",
+      "email"     : "carlos@loscar.com",
+      "password"  : "iofin45jhujv4dsidnhj",
+      "team"      : [],
+      "task"      : [3]
+    }
+  ],
+  "team" :[
+    {
+      "title" : "Cycorp",
+      "members" : [0,1]
+    }
+  ],
+  "task" : [
+    {
+      "title"      : "Faire à manger",
+      "created-on" : /*timestamp*/,
+      "done"       : false,
+      "done-on"    : null,
+      "user"       : [0]
+    },
+    {
+      "title"      : "Faire la vaisselle",
+      "created-on" : /*timestamp*/,
+      "done"       : false,
+      "done-on"    : null,
+      "user"       : [0,1]
+    },
+    {
+      "title"      : "Sortir les poubelles",
+      "created-on" : /*timestamp*/,
+      "done"       : false,
+      "done-on"    : null,
+      "user"       : [1]
+    }
+  ]
+}
